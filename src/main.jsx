d4387906db33a52204fed1bd74466d664fec6bde
Francisco Lago <vetting.ar@gmail.com>
2026-09-23 02:20:16 -0300

    Correccion de enrutamiento

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basenname="/c26242React-tp">
       <App />
     </BrowserRouter>
   </StrictMode>,
