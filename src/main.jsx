7bb1530dd9b8a22292cbfc2deb054802106b684f
Francisco Lago <vetting.ar@gmail.com>
2026-09-23 13:38:33 -0300

    Corrección de sintaxis

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basenname="/c26242React-tp">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
